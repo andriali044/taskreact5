df96dafc2f0474d56c1b473e65694dec91952417
andriali044 <andriali044gmail.com>
2022-02-22 18:48:53 +0700

    taskandri

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Statefull from './Statefull';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Statefull/>
   </React.StrictMode>,
   document.getElementById('root')
 );
